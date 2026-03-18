06ab8f1fabfba01644da1caed17c218dee3df292
Subrahmanya <subrahmanya.kp@qure.ai>
2026-03-18 06:44:54 +0530

    Created some endpoints and mcp layer on top of it

---
+++ mcp_server.py
@@ -0,0 +1,20 @@
+from fastmcp import FastMCP
+import httpx
+ 
+
+mcp = FastMCP(name="RPI MCP", version="1.0.0")
+
+@mcp.tool
+def market_mood():
+    """
+    Get the market mood index
+    """
+    return httpx.get("http://localhost:8005/mmi").json()
+
+@mcp.tool()
+def get_investable_bonds() -> list:
+    """Get investable secondary bonds"""
+    return httpx.get("http://localhost:8005/bonds").json()
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8006)
